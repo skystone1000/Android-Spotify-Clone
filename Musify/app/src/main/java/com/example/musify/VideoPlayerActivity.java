65c8689b30dc3213d975dd6e0cfc59ac73bc6256
Aditya <adityaspmahajan@gmail.com>
2021-11-29 03:47:33 +0530

    Basic Video Player with Controls

--- Musify/app/src/main/java/com/example/musify/VideoPlayerActivity.java
+++ Musify/app/src/main/java/com/example/musify/VideoPlayerActivity.java
@@ -3,6 +3,8 @@ package com.example.musify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class VideoPlayerActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class VideoPlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
+
+        VideoView videoPlayer = findViewById(R.id.videoView);
+        videoPlayer.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.birds);
+        MediaController mediaController = new MediaController(this);
+        videoPlayer.setMediaController(mediaController);
+        mediaController.setAnchorView(videoPlayer);
+        videoPlayer.start();
     }
 }
