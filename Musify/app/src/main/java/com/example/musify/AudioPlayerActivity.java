c4566e9d3d6ae4ac749e8b82b72522e70abaa45d
Aditya <adityaspmahajan@gmail.com>
2021-11-29 03:43:35 +0530

    Added Audio and Video Player structure

---
+++ Musify/app/src/main/java/com/example/musify/AudioPlayerActivity.java
@@ -0,0 +1,14 @@
+package com.example.musify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AudioPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_player);
+    }
+}
--- Musify/app/src/main/java/com/example/musify/MainActivity.java
+++ Musify/app/src/main/java/com/example/musify/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.musify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goToAudioPlayer(View view){
+        // Creating a Toast
+        Context context = getApplicationContext();
+        CharSequence text = "Audio Player starting!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+        // Building an Intent
+        Intent intent = new Intent(this, AudioPlayerActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToVideoPlayer(View view){
+        // Creating a Toast
+        Context context = getApplicationContext();
+        CharSequence text = "Video Player starting!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+        // Building an Intent
+        Intent intent = new Intent(this, VideoPlayerActivity.class);
+        startActivity(intent);
+    }
+
 }
